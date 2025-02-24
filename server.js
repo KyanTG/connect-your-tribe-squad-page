7f6467c30dec86106c1d5a70ea44e22f241c9ddb
KyanTG <kyan.gijtenbeek@hva.nl>
2025-02-24 11:50:22 +0100

    test page for messages

--- server.js
+++ server.js
@@ -105,3 +105,19 @@ app.listen(app.get('port'), function () {
   // Toon een bericht in de console en geef het poortnummer door
   console.log(`Application started on http://localhost:${app.get('port')}`)
 })
+
+// oefening
+
+let messages = []
+
+app.get('/berichten', async function (request, response) {
+
+  response.render('messages.liquid', {messages: messages})
+})
+
+app.post('/berichten', async function (request, response) {
+
+  messages.push(request.body.tekstje)
+
+  response.redirect(303, '/berichten')
+})
